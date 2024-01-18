f259df48669a91e29fbc80f26692239b958d5f19
Arthur C <arthurdourado01@hotmail.com>
2024-01-18 17:56:31 -0300

    componente de tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
-
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
 
+  const clientes = [
+    new Cliente('1', 'Ana', 34),
+    new Cliente('2', 'Arthur', 28),
+    new Cliente('3', 'Carlos', 12),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+    
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente.nome + ' excluído (a)');
+  }
+
   return (
-    <div className={`
-      flex justify-center items-center h-screen bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
-    `}>
-      <span className="text-4xl">Texto</span>
+    <div className={`flex justify-center items-center h-screen bg-gradient-to-r 
+    from-blue-500 to-purple-500 text-white`}>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+      </Layout>
     </div>
   )
 }
